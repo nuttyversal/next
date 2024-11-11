9576cca0ac1919ac322ef38c7d80f7b754e991ce
nuttyversal <mail@nuttyver.se>
2024-11-10 19:57:44 -0700

    Create flake for Lua tooling

---
+++ dev/languages/lua/flake.nix
@@ -0,0 +1,44 @@
+{
+	description = "Nutty's tools for Lua development";
+
+	inputs = {
+		nixpkgs = {
+			url = "github:NixOS/nixpkgs/nixos-unstable";
+		};
+
+		flake-utils = {
+			url = "github:numtide/flake-utils";
+		};
+
+		nixfmtty = {
+			url = "github:nuttyversal/next?dir=nix/pkgs/nixfmtty";
+		};
+	};
+
+	outputs =
+		{
+			self,
+			flake-utils,
+			nixpkgs,
+			nixfmtty,
+		}:
+		flake-utils.lib.eachDefaultSystem (
+			system:
+			let
+				pkgs = import nixpkgs {
+					inherit system;
+				};
+			in
+			{
+				devShells = {
+					default = pkgs.mkShell {
+						buildInputs = [
+							nixfmtty.packages.${system}.default
+							pkgs.lua-language-server
+							pkgs.stylua
+						];
+					};
+				};
+			}
+		);
+}
