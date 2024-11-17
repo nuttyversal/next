ce0e41f943705c649a335e9fa7f30b0e137bf3ad
nuttyversal <mail@nuttyver.se>
2024-11-17 15:52:20 -0700

    Configure SSH host keys

--- ops/nuttybox/system/openssh.nix
+++ ops/nuttybox/system/openssh.nix
@@ -5,6 +5,13 @@
 		openssh = {
 			enable = true;
 
+			hostKeys = [
+				{
+					path = "/sys/ssh/ed25519.key";
+					type = "ed25519";
+				}
+			];
+
 			# Send 💓 every 60 seconds to prevent timeouts.
 			extraConfig = "ClientAliveInterval 60";
 
