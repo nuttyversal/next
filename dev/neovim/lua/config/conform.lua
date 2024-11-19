eb0e15c1d45c1734640ee2aa183b1853675a830e
nuttyversal <mail@nuttyver.se>
2024-11-19 15:56:52 -0700

    Configure `nixfmtty` as Nix formatter

--- dev/neovim/lua/config/conform.lua
+++ dev/neovim/lua/config/conform.lua
@@ -1,8 +1,19 @@
 local conform = require("conform")
 
 conform.setup({
+	formatters = {
+		nixfmtty = {
+			command = "nixfmtty",
+			args = { "$FILENAME" },
+
+			-- Modifies in-place.
+			stdin = false,
+		},
+	},
+
 	formatters_by_ft = {
 		lua = { "stylua" },
+		nix = { "nixfmtty" },
 	},
 
 	format_on_save = {
