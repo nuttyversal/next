cf70290b1af06289e24efccdedc8cc80ab613734
nuttyversal <mail@nuttyver.se>
2024-11-20 16:46:58 -0700

    Trust Caddy's internal CA certificate

--- ops/nuttybox/system/networking.nix
+++ ops/nuttybox/system/networking.nix
@@ -44,4 +44,26 @@
 			};
 		};
 	};
+
+	security = {
+		pki = {
+			certificates = [
+				# Trust Caddy's internal CA certificate in nuttygate.
+				# /var/lib/caddy/.local/share/caddy/pki/authorities/local/root.crt
+				''
+					-----BEGIN CERTIFICATE-----
+					MIIBpDCCAUqgAwIBAgIRAK21T113dh0jNjmTO42aSJwwCgYIKoZIzj0EAwIwMDEu
+					MCwGA1UEAxMlQ2FkZHkgTG9jYWwgQXV0aG9yaXR5IC0gMjAyNCBFQ0MgUm9vdDAe
+					Fw0yNDExMjAyMzMxMDJaFw0zNDA5MjkyMzMxMDJaMDAxLjAsBgNVBAMTJUNhZGR5
+					IExvY2FsIEF1dGhvcml0eSAtIDIwMjQgRUNDIFJvb3QwWTATBgcqhkjOPQIBBggq
+					hkjOPQMBBwNCAARjt8bU1QaG6giGvsTKAaMxkzOcanQs0tdrNILXiQeFmD7IHdky
+					XGg0e3ZzdTUhgPtE0y8Y8p4HVBazd3vMOyBvo0UwQzAOBgNVHQ8BAf8EBAMCAQYw
+					EgYDVR0TAQH/BAgwBgEB/wIBATAdBgNVHQ4EFgQUjQqbZXdQpptlLoenQ8gDc/rI
+					VH4wCgYIKoZIzj0EAwIDSAAwRQIgKPI9pPkB/G4u8HJ933rM4UXs82o5sBmsspFT
+					4bLcR7UCIQCz6/0saJNss2EVC1XsmBk6vzHQcTSfDZUBuGsKHWgLzw==
+					-----END CERTIFICATE-----
+				''
+			];
+		};
+	};
 }
