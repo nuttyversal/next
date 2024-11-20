4b40057fe34632d7db5d8aa9612590afc1bb1c8e
nuttyversal <mail@nuttyver.se>
2024-11-19 23:22:06 -0700

    Configure WireGuard network

--- ops/nuttybox/system/networking.nix
+++ ops/nuttybox/system/networking.nix
@@ -4,10 +4,44 @@
 	networking = {
 		hostName = "nuttybox";
 		hostId = "c5bb9702";
+
+		hosts = {
+			"10.100.0.1" = [ "nuttygate" ];
+		};
+
+		nameservers = [
+			# nuttygate
+			"10.100.0.1"
+
+			# Cloudflare
+			"1.1.1.1"
+
+			# Google
+			"8.8.8.8"
+		];
+
 		useDHCP = true;
 
 		firewall = {
 			enable = true;
+		};
+
+		wireguard = {
+			interfaces = {
+				wg0 = {
+					ips = [ "10.100.0.2/24" ];
+					privateKeyFile = "/host/wireguard/private";
+
+					peers = [
+						{
+							publicKey = "/JuoCrhAu+x3x4yXiVxSd0Zd8iDOzxIQKMFNNIHLjkI=";
+							endpoint = "nuttyver.se:51820";
+							allowedIPs = [ "10.100.0.0/24" ];
+							persistentKeepalive = 30;
+						}
+					];
+				};
+			};
 		};
 	};
 }
--- ops/nuttygate/system/networking.nix
+++ ops/nuttygate/system/networking.nix
@@ -5,9 +5,13 @@
 		hostName = "nuttygate";
 
 		nameservers = [
+			# Hetzner DNS resolvers (IPv6)
 			"2a01:4ff:ff00::add:1"
 			"2a01:4ff:ff00::add:2"
+
+			# Hetzner DNS resolvers (IPv4)
 			"185.12.64.1"
+			"185.12.64.2"
 		];
 
 		defaultGateway = "172.31.1.1";
@@ -62,11 +66,73 @@
 				};
 			};
 		};
+
+		wireguard = {
+			interfaces = {
+				wg0 = {
+					ips = [ "10.100.0.1/24" ];
+					listenPort = 51820;
+					privateKeyFile = "/host/wireguard/private";
+
+					peers = [
+						{
+							publicKey = "JD3snzuU2Poi/SRnc96D11bZVGUwB0fY3k+cyzsXf2I=";
+							allowedIPs = [ "10.100.0.2/24" ];
+							persistentKeepalive = 30;
+						}
+					];
+				};
+			};
+		};
+
+		firewall = {
+			enable = true;
+
+			# Listen for WireGuard connections.
+			allowedUDPPorts = [ 51820 ];
+
+			extraCommands = ''
+				# Allow nuttynet clients to access the internet by masquerading as
+				# as the gateway's public IP via network address translation (NAT).
+				iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+			'';
+		};
 	};
 
 	services = {
+		coredns = {
+			enable = true;
+
+			config = ''
+				. {
+					hosts {
+						# Nutty Network (nuttynet)
+						10.100.0.1 nuttygate
+						10.100.0.2 nuttybox
+						10.100.0.3 nuttynas
+						10.100.0.4 nuttybook
+						10.100.0.5 nuttytower
+						fallthrough
+					}
+				}
+
+				forward . {
+					# Hetzner DNS resolvers (IPv6)
+					2a01:4ff:ff00::add:1
+					2a01:4ff:ff00::add:2
+
+					# Hetzner DNS resolvers (IPv4)
+					185.12.64.1
+					185.12.64.2
+				}
+
+				cache
+			'';
+		};
+
 		udev = {
 			extraRules = ''
+				# Ensure network interface is named eth0.
 				ATTR{address}=="96:00:03:dc:32:f8", NAME="eth0"
 			'';
 		};
