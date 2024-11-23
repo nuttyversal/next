05d25b0bdd480291abd8980f1723db54f3387de9
nuttyversal <mail@nuttyver.se>
2024-11-23 12:27:34 -0700

    Try to bind to WireGuard IP address

--- ops/nuttybox/services/certificate-authority.nix
+++ ops/nuttybox/services/certificate-authority.nix
@@ -7,7 +7,7 @@
 				nutty-ca = {
 					image = "smallstep/step-ca";
 					volumes = [ "/data/certificates:/home/step" ];
-					ports = [ "8443:9000" ];
+					ports = [ "10.100.0.2:8443:9000" ];
 				};
 			};
 		};
