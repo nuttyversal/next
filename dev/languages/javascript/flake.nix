f1da1589a8f05aa1ee34878aa30021c4304662c9
nuttyversal <mail@nuttyver.se>
2024-11-28 16:38:06 -0700

    Install web development tooling

---
+++ dev/languages/javascript/flake.nix
@@ -0,0 +1,49 @@
+{
+	description = "Nutty's tools for JS/TS development";
+
+	inputs = {
+		nixpkgs = {
+			url = "github:NixOS/nixpkgs/nixos-unstable";
+		};
+
+		flake-utils = {
+			url = "github:numtide/flake-utils";
+		};
+
+		nixfmtty = {
+			url = "github:nuttyversal/next?dir=nix/pkgs/nixfmtty";
+		};
+	};
+
+	outputs =
+		{
+			self,
+			flake-utils,
+			nixpkgs,
+			nixfmtty,
+		}:
+		flake-utils.lib.eachDefaultSystem (
+			system:
+			let
+				pkgs = import nixpkgs {
+					inherit system;
+				};
+			in
+			{
+				devShells = {
+					default = pkgs.mkShell {
+						buildInputs = [
+							nixfmtty.packages.${system}.default
+							pkgs.nodejs
+							pkgs.nodePackages.eslint
+							pkgs.nodePackages.pnpm
+							pkgs.nodePackages.prettier
+							pkgs.nodePackages.sass
+							pkgs.nodePackages.ts-node
+							pkgs.nodePackages.typescript
+						];
+					};
+				};
+			}
+		);
+}
