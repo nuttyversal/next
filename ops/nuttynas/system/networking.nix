2639c2d01ac07141f1d044f486eaf193c3ce6ad5
nuttyversal <mail@nuttyver.se>
2024-12-28 21:32:05 -0700

    Update renamed NixOS options for Samba

--- ops/nuttynas/system/networking.nix
+++ ops/nuttynas/system/networking.nix
@@ -42,46 +42,48 @@
 
 		samba = {
 			enable = true;
-			securityType = "user";
 
 			settings = {
-				"workgroup" = "WORKGROUP";
-				"server string" = "NAS";
-				"server role" = "standalone server";
-				"map to guest" = "bad user";
-			};
-
-			shares = {
-				"nutty" = {
-					"path" = "/nas/unshared/nutty";
-					"valid users" = "nutty";
-					"force user" = "nutty";
-					"force group" = "users";
-					"read only" = "no";
-					"browseable" = "yes";
-					"create mask" = "0644";
-					"directory mask" = "0755";
+				global = {
+					"security" = "user";
+					"workgroup" = "WORKGROUP";
+					"server string" = "NAS";
+					"server role" = "standalone server";
+					"map to guest" = "bad user";
 				};
 
-				"emily" = {
-					"path" = "/nas/unshared/emily";
-					"valid users" = "emily";
-					"force user" = "emily";
-					"force group" = "users";
-					"read only" = "no";
-					"browseable" = "yes";
-					"create mask" = "0644";
-					"directory mask" = "0755";
-				};
+				shares = {
+					"nutty" = {
+						"path" = "/nas/unshared/nutty";
+						"valid users" = "nutty";
+						"force user" = "nutty";
+						"force group" = "users";
+						"read only" = "no";
+						"browseable" = "yes";
+						"create mask" = "0644";
+						"directory mask" = "0755";
+					};
 
-				"shared" = {
-					"path" = "/nas/shared";
-					"valid users" = "@family";
-					"force group" = "family";
-					"read only" = "no";
-					"browseable" = "yes";
-					"create mask" = "0664";
-					"directory mask" = "0775";
+					"emily" = {
+						"path" = "/nas/unshared/emily";
+						"valid users" = "emily";
+						"force user" = "emily";
+						"force group" = "users";
+						"read only" = "no";
+						"browseable" = "yes";
+						"create mask" = "0644";
+						"directory mask" = "0755";
+					};
+
+					"shared" = {
+						"path" = "/nas/shared";
+						"valid users" = "@family";
+						"force group" = "family";
+						"read only" = "no";
+						"browseable" = "yes";
+						"create mask" = "0664";
+						"directory mask" = "0775";
+					};
 				};
 			};
 		};
