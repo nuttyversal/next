e8c030d8b631641407320667aebd134c8f13eeee
nuttyversal <mail@nuttyver.se>
2025-01-26 17:24:24 -0700

    Add git configuration

---
+++ dev/git/flake.nix
@@ -0,0 +1,45 @@
+{
+	description = "Nutty's version control tools";
+
+	inputs = {
+		nixpkgs = {
+			url = "github:NixOS/nixpkgs/nixos-unstable";
+		};
+
+		flake-utils = {
+			url = "github:numtide/flake-utils";
+		};
+
+		nixfmtty = {
+			url = "github:nuttyversal/next?dir=nix/pkgs/nixfmtty";
+		};
+	};
+
+	outputs =
+		{
+			self,
+			flake-utils,
+			nixpkgs,
+			nixfmtty,
+		}:
+		flake-utils.lib.eachDefaultSystem (
+			system:
+			let
+				pkgs = import nixpkgs {
+					inherit system;
+				};
+			in
+			{
+				devShells = {
+					default = pkgs.mkShell {
+						buildInputs = [
+							nixfmtty.packages.${system}.default
+							pkgs.coreutils
+							pkgs.difftastic
+							pkgs.git
+						];
+					};
+				};
+			}
+		);
+}
