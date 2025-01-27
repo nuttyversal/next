ad5cbe5b9c245af426bca2150c7a8734f45ab4dd
nuttyversal <mail@nuttyver.se>
2025-01-26 17:24:42 -0700

    Add KMonad configuration

---
+++ lab/kmonad/flake.nix
@@ -0,0 +1,44 @@
+{
+	description = "KMonad advanced keyboard manager";
+
+	inputs = {
+		kmonad = {
+			url = "git+https://github.com/kmonad/kmonad?submodules=1&dir=nix";
+		};
+	};
+
+	outputs =
+		{
+			self,
+			nixpkgs,
+			kmonad,
+		}:
+		let
+			kmonadPackage = kmonad.packages.aarch64-darwin.kmonad;
+			config = nixpkgs.legacyPackages.x86_64-linux.writeText (builtins.readFile ./macos.kbd);
+		in
+		rec {
+			apps = {
+				aarch64-darwin = {
+					default = {
+						type = "app";
+						program = "${kmonadPackage}/bin/kmonad";
+					};
+				};
+			};
+
+			packages = {
+				aarch64-darwin = {
+					kmonad = kmonadPackage;
+				};
+			};
+
+			devShells = {
+				aarch64-darwin = {
+					default = nixpkgs.legacyPackages.aarch64-darwin.mkShell {
+						buildInputs = nixpkgs.lib.attrsets.attrValues packages.aarch64-darwin;
+					};
+				};
+			};
+		};
+}
