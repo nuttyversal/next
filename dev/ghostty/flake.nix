776fba1879e7d36b1351be549313cd5985dd2896
nuttyversal <mail@nuttyver.se>
2025-01-27 23:01:31 -0700

    Add Ghostty configuration

---
+++ dev/ghostty/flake.nix
@@ -0,0 +1,43 @@
+{
+	description = "Nutty's terminal";
+
+	inputs = {
+		nixpkgs = {
+			url = "github:NixOS/nixpkgs/nixos-unstable";
+		};
+
+		flake-utils = {
+			url = "github:numtide/flake-utils";
+		};
+
+		nixfmtty = {
+			url = "github:nuttyversal/next?dir=nix/pkgs/nixfmtty";
+		};
+	};
+
+	outputs =
+		{
+			self,
+			flake-utils,
+			nixpkgs,
+			nixfmtty,
+		}:
+		flake-utils.lib.eachDefaultSystem (
+			system:
+			let
+				pkgs = import nixpkgs {
+					inherit system;
+				};
+			in
+			{
+				devShells = {
+					default = pkgs.mkShell {
+						buildInputs = [
+							nixfmtty.packages.${system}.default
+							pkgs.coreutils
+						];
+					};
+				};
+			}
+		);
+}
