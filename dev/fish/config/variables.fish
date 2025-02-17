e7106f95b3ac9074d48ecac818d884e1a7f95b37
nuttyversal <mail@nuttyver.se>
2025-02-17 15:43:40 -0700

    Initialize Zoxide when fish starts up

--- dev/fish/config/variables.fish
+++ dev/fish/config/variables.fish
@@ -5,6 +5,9 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 set --global --export GPG_TTY (tty)
 set --global --export GNUPGHOME ~/.config/gnupg
 
+# Zoxide
+zoxide init fish | source
+
 # Nix
 if test -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
 	. '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
