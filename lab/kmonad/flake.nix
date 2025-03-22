7c3224e216fa7eaceb0ad82e82dc68b7ebee30a4
nuttyversal <mail@nuttyver.se>
2025-03-22 16:23:35 -0700

    Add dependencies to kmonad flake

--- lab/kmonad/flake.nix
+++ lab/kmonad/flake.nix
@@ -2,8 +2,18 @@
 	description = "KMonad advanced keyboard manager";
 
 	inputs = {
+		nixpkgs = {
+			url = "github:NixOS/nixpkgs/nixos-unstable";
+		};
+
 		kmonad = {
+			# This doesn't work under the `just ci` context.
+			# fatal: https://github.com/kmonad/kmonad?dir=nix/info/refs not valid: is this a git repository?
 			url = "git+https://github.com/kmonad/kmonad?submodules=1&dir=nix";
+		};
+
+		nixfmtty = {
+			url = "github:nuttyversal/next?dir=nix/pkgs/nixfmtty";
 		};
 	};
 
@@ -11,11 +21,13 @@
 		{
 			self,
 			nixpkgs,
+			nixfmtty,
 			kmonad,
 		}:
 		let
+			pkgs = nixpkgs.legacyPackages.aarch64-darwin;
 			kmonadPackage = kmonad.packages.aarch64-darwin.kmonad;
-			config = nixpkgs.legacyPackages.x86_64-linux.writeText (builtins.readFile ./macos.kbd);
+			config = pkgs.writeText (builtins.readFile ./macos.kbd);
 		in
 		rec {
 			apps = {
@@ -36,7 +48,14 @@
 			devShells = {
 				aarch64-darwin = {
 					default = nixpkgs.legacyPackages.aarch64-darwin.mkShell {
-						buildInputs = nixpkgs.lib.attrsets.attrValues packages.aarch64-darwin;
+						buildInputs = nixpkgs.lib.attrsets.attrValues packages.aarch64-darwin ++ [
+							nixfmtty.packages.aarch64-darwin.default
+							pkgs.coreutils
+							pkgs.fish
+							pkgs.git
+							pkgs.just
+							pkgs.nix
+						];
 					};
 				};
 			};
