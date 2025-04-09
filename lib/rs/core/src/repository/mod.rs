b51b5466839e8ffe73fd47e65b5b9d0982d48d2e
nuttyversal <mail@nuttyver.se>
2025-04-08 20:42:57 -0700

    Add re-exports for convenience

--- lib/rs/core/src/repository/mod.rs
+++ lib/rs/core/src/repository/mod.rs
@@ -2,3 +2,7 @@ pub mod memory;
 pub mod postgres;
 pub mod tests;
 pub mod traits;
+
+pub use memory::MemoryContentRepository;
+pub use postgres::PostgresContentRepository;
+pub use traits::ContentRepository;
