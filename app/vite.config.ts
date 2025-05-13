68f4a5b30711381d3256447a3e758da92a3f1974
nuttyversal <mail@nuttyver.se>
2025-05-13 14:25:32 -0700

    Output bundle visualization after build

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
+import { visualizer } from "rollup-plugin-visualizer";
 import solidPlugin from "vite-plugin-solid";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-	plugins: [solidPlugin(), tailwindcss()],
+	plugins: [
+		solidPlugin(),
+		tailwindcss(),
+		visualizer({
+			open: true,
+			gzipSize: true,
+		}),
+	],
 	server: {
 		port: 3000,
 	},
