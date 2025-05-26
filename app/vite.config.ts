4737845038298830fe9b975638700baf5546cb71
nuttyversal <mail@nuttyver.se>
2025-05-26 12:51:47 -0700

    Remove `localhost` from `allowedHosts`

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
 		}),
 	],
 	server: {
-		allowedHosts: ["localhost", "local.nuttyver.se"],
+		allowedHosts: ["local.nuttyver.se"],
 		host: "0.0.0.0",
 		port: 3000,
 	},
